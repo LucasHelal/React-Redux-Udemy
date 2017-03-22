9e51936062dcc501ab700f3c6411f50d8fe5e7be
Lucas Helal <lucasghelal@gmail.com>
2017-03-22 02:57:46 -0300

    Criação do componente de grid

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader';
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small='Nós'></PageHeader>
+
+        <h2> Nossa História </h2>
+        <p> Lorem ipsum sit amet... </p>
+        <h2> Missão e Visão </h2>
+        <p> Lorem ipsum sit amet... </p>
+        <h2> Imprensaã </h2>
+        <p> Lorem ipsum sit amet... </p>
+    </div>
+)
