ed4c756c68d05b9cf308fa4eda64b14bbdc97c92
Lucas Helal <lucasghelal@gmail.com>
2017-03-22 03:40:06 -0300

    inserção funcionando no banco

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import Grid from '../template/grid';
+import IconButtom from '../template/iconButton';
 
 export default props => (
     <div>
-        <div className='col-xs-12 col-sm-9 col-md-10'>
+        <Grid cols='12 9 10'>
             <input id='description' className='form-control'
-                placeholder='Adicione uma tarefa'></input>
-        </div>
+                placeholder='Adicione uma tarefa'
+                onChange={props.handleChange}
+                value={props.description}></input>
+        </Grid>
 
-        <div className='col-xs-12 col-sm-3 col-md-2'>
-            <button className='btn btn-primary'>
-                <i className='fa fa-plus '></i>
-            </button>
-        </div>
+        <Grid cols='12 3 2'>
+            <IconButtom style='primary' icon='plus'
+                onClick={props.handleAdd}></IconButtom>
+        </Grid>
     </div>
 
 );
